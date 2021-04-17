12db8964d9edeace4132854387de591580fdb782
Oscar Fridh <oscar.fridh.mkk@gmail.com>
2021-04-17 17:53:49 +0200

    Added intro page

---
+++ SwiftSearch.playground/Pages/Intro.xcplaygroundpage/Contents.swift
@@ -0,0 +1,19 @@
+
+/*:
+ # Swift Search
+ In this playground we will explore and code a few search algorithms together step by step in Swift. You will implement the search function and see how it operates as you run your code. The UX is very much inspired by Bytes in the Swift playground app and I think it is a great way of learning. I hope you will enjoy it!
+ 
+ ## Table of content
+ 1. Linked list
+ 2. DAG
+ 3. Cycles
+ 4. DFS
+ 5. BFS
+ 6. DFS vs BFS
+ 7. Wrap up
+ 
+ Let's get started!
+ 
+ [Next](@next)
+ 
+ */
