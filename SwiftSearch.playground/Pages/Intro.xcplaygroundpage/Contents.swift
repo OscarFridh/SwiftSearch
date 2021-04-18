6d81cdb1b0878e12b014aac58bd4ed9cdd9893c1
Oscar Fridh <oscar.fridh.mkk@gmail.com>
2021-04-18 18:47:16 +0200

    Added missing pages and set up links in the table of content

--- SwiftSearch.playground/Pages/Intro.xcplaygroundpage/Contents.swift
+++ SwiftSearch.playground/Pages/Intro.xcplaygroundpage/Contents.swift
@@ -4,15 +4,13 @@
  In this playground we will explore and code a few search algorithms together step by step in Swift. You will implement the search function and see how it operates as you run your code. The UX is very much inspired by Bytes in the Swift playground app and I think it is a great way of learning. I hope you will enjoy it!
  
  ## Table of content
- 1. Linked list
- 2. DAG
- 3. Cycles
- 4. DFS
- 5. BFS
- 6. DFS vs BFS
- 7. Wrap up
- 
- I know that you only have 3 minutes
+ 1. [Linked List](LinkedList)
+ 2. [Directed Asyclic Graph](DAG)
+ 3. [Cycles](Cycles)
+ 4. [Depth First Search](DFS)
+ 5. [Breadth First Search](BFS)
+ 6. [DFS vs BFS](DFSvsBFS)
+ 7. [Thank you](ThankYou)
  
  Let's get started!
  
