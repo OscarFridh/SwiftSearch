88b51965c6fdc729206af029f2f7bb5fb75141ed
Oscar Fridh <oscar.fridh.mkk@gmail.com>
2021-04-18 20:26:30 +0200

    Created larger graph scene

--- SwiftSearch.playground/Pages/DFSvsBFS.xcplaygroundpage/Contents.swift
+++ SwiftSearch.playground/Pages/DFSvsBFS.xcplaygroundpage/Contents.swift
@@ -9,7 +9,7 @@ import PlaygroundSupport
 
 let searchAlgorithms = [dfs, bfs]
 
-let view = View.create(target: "F", searchAlgorithm: searchAlgorithms[0], speed: 1)
+let view = View.create(target: "J", searchAlgorithm: searchAlgorithms[1], speed: 1)
 // TODO: Stack or change SKScene on completion with a custom delegate!
 PlaygroundSupport.PlaygroundPage.current.liveView = view
 
--- SwiftSearch.playground/Pages/DFSvsBFS.xcplaygroundpage/Sources/Facade.swift
+++ SwiftSearch.playground/Pages/DFSvsBFS.xcplaygroundpage/Sources/Facade.swift
@@ -4,17 +4,28 @@ import SpriteKit
 extension View {
     
     static let graph = Graph(nodes: [
-        .init(id: "a", value: "A", neighbors: ["b", "d"]),
-        .init(id: "b", value: "B", neighbors: ["c"]),
-        .init(id: "c", value: "C", neighbors: []),
-        .init(id: "d", value: "D", neighbors: ["e", "f"]),
-        .init(id: "e", value: "E", neighbors: []),
-        .init(id: "f", value: "F", neighbors: ["a"]),
+        .init(id: "a", value: "A", neighbors: ["b"]),
+        .init(id: "b", value: "B", neighbors: ["c", "k", "m"]),
+        .init(id: "c", value: "C", neighbors: ["d"]),
+        .init(id: "d", value: "D", neighbors: ["e"]),
+        .init(id: "e", value: "E", neighbors: ["f"]),
+        .init(id: "f", value: "F", neighbors: ["g"]),
+        .init(id: "g", value: "G", neighbors: ["h"]),
+        .init(id: "h", value: "H", neighbors: ["i", "b"]),
+        .init(id: "i", value: "I", neighbors: ["j"]),
+        .init(id: "j", value: "J", neighbors: ["p"]),
+        .init(id: "k", value: "K", neighbors: ["l"]),
+        .init(id: "l", value: "L", neighbors: ["f"]),
+        .init(id: "m", value: "M", neighbors: ["n"]),
+        .init(id: "n", value: "N", neighbors: ["o"]),
+        .init(id: "o", value: "O", neighbors: ["j"]),
+        .init(id: "p", value: "P", neighbors: ["g"]),
     ])
     
     public static func create(target: String, searchAlgorithm: SearchAlgorithm, speed: Double) -> UIView {
         let level = Level(graph: graph, targetValue: target)
         let searchResult = level.search(using: searchAlgorithm)
+        print(searchResult.path)
         return View.create(graph: level.graph, searchResult: searchResult, speed: speed)
     }
 }
