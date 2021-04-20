986f00f7417b26ff7a5b0ce0da7bb4114d24d907
Oscar Fridh <oscar.fridh.mkk@gmail.com>
2021-04-20 06:38:04 +0200

    Fixed LinkedList -> Cycles

--- SwiftSearch.playground/Pages/Cycles.xcplaygroundpage/Contents.swift
+++ SwiftSearch.playground/Pages/Cycles.xcplaygroundpage/Contents.swift
@@ -1,40 +1,77 @@
 /*:
  [Previous](@previous)
  # Cycles
- When searching a graph with cycles it is important to avoid getting stuck in an infinite loop.
- In order to do this we mark nodes as discovered as we visit them.
- If a neighbor has already been discovered we can avoid visiting it again and thereby escaping the cycle.
+ Let's point the end to the start of the list so that it forms a loop.
+ This causes our current algorithm to get stuck forever.
  
- Discovered nodes are shown in purple.
+ In order to fix it we need to:
+ 1. Mark nodes as discovered when we visit them
+ 2. Avoid visiting nodes that have already been discovered
+ 
+ Please fix the function below.
+ 
+ Scroll down to see an example solution ↓
  */
 
 import PlaygroundSupport
 
-// TODO: Ev. Förklara varför try behövs?
+
+
 
 func findPath(from node: Node, to emoji: String) throws -> [Node] {
-    node.discovered = true
     if try node.checkEmoji() == emoji {
         return [node]
     }
-    if node.neighbor?.discovered == false {
-        let path = try findPath(from: node.neighbor!, to: emoji)
-        if path.count > 0 {
-            return [node] + path
+    if let nextNode = node.neighbor {
+        let remainingPath = try findPath(from: nextNode, to: emoji)
+        if remainingPath.count > 0 {
+            return [node] + remainingPath
         }
     }
     return []
 }
+
+/*:
+ >Exceptions are only used to escape infinate loops
+*/
+
 
 
 // The following emojis can be found: 🐒🙋🏻‍♂️🤖
 let emoji = "🏆"
 let searchResult = Level.search(for: emoji, using: findPath)
 let view = View.create(searchResult: searchResult, speed: 1)
-// TODO: view.colors.discoveredCircle = #colorliteral ...
 PlaygroundSupport.PlaygroundPage.current.liveView = view
+
+
+
+
+
+
+
+
+
 /*:
- If you try to find the path to an emoji that is not present, the algorithm ends up searching the entire graph before it returns. In that case there is no path to the target (represented by an empty array) and no path to show in green (or red) at the end.
+ Copy and paste to quickly see a solution.
+ Replace the function above with the following lines of code:
+ */
+
+/*:
+     func findPath(from node: Node, to emoji: String) throws -> [Node] {
+         node.discovered = true
+         if try node.checkEmoji() == emoji {
+             return [node]
+         }
+         if let nextNode = node.neighbor {
+             if nextNode.discovered == false {
+                 let remainingPath = try findPath(from: nextNode, to: emoji)
+                 if remainingPath.count > 0 {
+                     return [node] + remainingPath
+                 }
+             }
+         }
+         return []
+     }
  */
 
 //: [Next](@next)
--- SwiftSearch.playground/Pages/LinkedList.xcplaygroundpage/Contents.swift
+++ SwiftSearch.playground/Pages/LinkedList.xcplaygroundpage/Contents.swift
@@ -3,28 +3,29 @@
  # Linked List
  
  We are given a list of nodes where each node hides an emoji.
- Our job is to find a path from a starting node to a certain emoji 🐒
+ Our job is to find a path from a given node to a certain emoji 🐒
  
- Please finish implementing the function below to do that recursively.
+ Please finish implementing the function below.
  
  Scroll down to see an example solution ↓
  */
 import PlaygroundSupport
 
+
 func findPath(from node: Node, to emoji: String) throws -> [Node] {
-    if try node.checkEmoji() == emoji {
-        return [node]
-    }
-    
-    /** TODO:
-     If the node has a neighbor:
-        Return a path starting with this node, followed by the remaining path returned from the neighbor.
-     otherwise:
-        We have reached the end of the list without finding the emoji, return []
-     */
-    
-    return []
-}
+     if try node.checkEmoji() == emoji {
+         return [node]
+     }
+     if let nextNode = node.neighbor {
+        /** TODO: Replace this with your code!
+         1. Find the remaining path from `nextNode`
+         2. If it was found (not empty),
+            Return a path starting with `node`, followed by the remaining path
+         */
+     }
+     return []
+ }
+
 
 let emoji = "🐒"
 let searchResult = Level.search(for: emoji, using: findPath)
@@ -45,7 +46,7 @@ PlaygroundSupport.PlaygroundPage.current.liveView = view
 
 /*:
  Copy and paste to quickly see a solution.
- Replace the function above with the following:
+ Replace the function above with the following lines of code:
  */
 
 /*:
@@ -54,7 +55,10 @@ PlaygroundSupport.PlaygroundPage.current.liveView = view
              return [node]
          }
          if let nextNode = node.neighbor {
-              return [node] + findPath(to: emoji, from: nextNode)
+             let remainingPath = try findPath(from: nextNode, to: emoji)
+             if remainingPath.count > 0 {
+                 return [node] + remainingPath
+             }
          }
          return []
      }
